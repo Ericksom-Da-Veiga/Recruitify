56ad0ad9a71d704f306b8a2af1250216b41e6ff4
Erickson-Da-Veiga <ericksondaveiga10@gmail.com>
2023-03-23 01:53:02 +0000

    junior

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './Components/Navbar.js';
-import Brand from './Images/logo.png';
-import Intro from './Components/Intro';
-
-import img1 from './Images/job1.jpg';
-import img2 from './Images/job2.jpg';
-import img3 from './Images/job3.jpg';
-import img4 from './Images/job4.jpg';
+import App from './App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Navbar img={Brand} link1='#' link2='#' link3='#' link4='#'/>
-    <Intro img1={img1} img2={img2} img3={img3} img4={img4} />
+    <App/>
   </div>
   
 );
