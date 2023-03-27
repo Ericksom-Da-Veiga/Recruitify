a79850917240582bf12f9942797663218b48ac73
Sanimi <103943591+Batale@users.noreply.github.com>
2023-03-27 13:58:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import {BrowserRouter as Router, Route ,Switch,Link} from "react-router-dom";
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import Home from "./index"
-import Profil from "./Pages/Profil/index"
-import Publier from "./Pages/Publier/index"
-import Emploi from "./Pages/Chercher/index"
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Notes from "./components/Notes";
+import CreateNote from "./components/CreateNote";
+
 
 function App() {
-  return (jhv
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/notes" element={<Notes/>}/>
+        <Route exact path="/create" element={<CreateNote/>}/>
+      </Routes>
+    </Router>
   );
 }
 
