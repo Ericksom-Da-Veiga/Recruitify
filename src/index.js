1f667edb63fa7cbabc9760373fbe78721940b769
Sanimi <103943591+Batale@users.noreply.github.com>
2023-03-27 13:59:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Navbar from './Components/Navbar';
-import Intro from './Components/Intro';
-import Categorie from './Components/Categorie';
-import Emploi from './Components/Emploi';
-import Fin from './Components/Fin'
-
-import Brand from './Images/logo.png';
-import img1 from './Images/job1.jpg';
-import img2 from './Images/job2.jpg';
-import img3 from './Images/job3.jpg';
-import img4 from './Images/job4.jpg';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-        <Navbar img={Brand} link1='#' link2='#' link3='#' link4='#'/>
-        <Intro img1={img1} img2={img2} img3={img3} img4={img4} />
-        <Categorie/>
-        <Emploi/>
-        <Fin/>
-  </div>
-  
+    <App />
 );
 
